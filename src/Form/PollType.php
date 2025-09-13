6120e347e2cbcad833864d260dd7d1b22e3d5ba7
kevin <kevin.progc@gmail.com>
2025-09-13 20:16:19 +0200

    Règles de validation formulaire choix sondage

--- src/Form/PollType.php
+++ src/Form/PollType.php
@@ -6,12 +6,12 @@ use App\Entity\Poll;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PollType extends AbstractType
 {
@@ -35,7 +35,23 @@ class PollType extends AbstractType
 
         $builder->add('choices', TextareaType::class,
         [
-            'mapped' => false
+            'mapped' => false,
+            "constraints" => new Assert\Callback(["callback" => static function (string $data, ExecutionContextInterface $context)
+            {
+                $nbItems = count(explode("\r\n", $data));
+
+                if($nbItems < 2)
+                {
+                    $context->buildViolation("Merci de définir minimum 2 choix par sondage")
+                    ->addViolation();
+                }
+
+                if($nbItems > 10)
+                {
+                    $context->buildViolation("Merci de définir maximum 10 choix par sondage")
+                    ->addViolation();
+                }
+            }])
         ]);
 
         $builder->add("save", SubmitType::class);
